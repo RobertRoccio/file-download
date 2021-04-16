70688f8490ce36f23cde5306ed35a15fc71ee051
RobertRoccio <robertroccio11@gmail.com>
2021-04-16 09:55:31 -0700

    Test Change

--- webapp/app.py
+++ webapp/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/download')
 def downloadFile ():
     path = "test_file.zip"
-    return send_file(path, as_attachment=True)
+    #return send_file(path, as_attachment=True)
+    return send_file(path)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
